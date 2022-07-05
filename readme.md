1cb07a615b8188e7b196fcf6d78b158c91bfaa51
arjun <arjunsreedar@yahoo.com>
2022-07-06 03:40:28 +0530

    initial

---
+++ readme.md
@@ -0,0 +1,21 @@
+# Telegram chat history exporter
+* A simple python script using the telethon library to extract message history of the specified telegram channel
+## Requirements
+* Telethon 1.24.0
+* dotenv
+## Environment variables
+* check the env.example file in the task1 directory and follow the same format to create the actual env file. Once done, you can run the code.
+* Note: The env file needs the api_id and api_hash which can be obtained by creating a developer account on telegram.
+## Process
+* Enter the link of a channel or its ID. The link is available on the description (mostly). The ID can be obtained from the url while     
+  using telegram desktop.
+* Make sure the channel isn't private.
+* Enter the limit (number of messages to download)
+## Output
+* Output is printed as well as stored in a JSON file
+* It is stored as a dict ({'user_id': ID', 'message': MESSAGE})
+## Side Notes
+* You will be asked to login for the first session.
+* You would have to enter your phone number in an international format like `+91XXXXXXXXXX`.
+* Followed by this, you'd have to enter a code that was sent to you through telegram.
+* You're good to go now. If any queries, feel free to contact me!
---
+++ telegram.py
@@ -0,0 +1,77 @@
+import os
+from dotenv import load_dotenv
+import json
+from telethon import TelegramClient
+from telethon.errors import SessionPasswordNeededError, ChannelPrivateError
+from telethon.tl.functions.messages import (GetHistoryRequest)
+from telethon.tl.types import (
+    PeerChannel
+)
+
+
+load_dotenv()
+
+
+api_id = os.getenv('api_id')
+api_hash = os.getenv('api_hash')
+phone = os.getenv('phone')
+username = os.getenv('username')
+print(username)
+
+async def extract_messages(phone):
+    await client.start()
+    # Ensure you're authorized
+    if await client.is_user_authorized() == False:
+        await client.send_code_request(phone)
+        try:
+            await client.sign_in(phone, input('Enter the code: '))
+        except SessionPasswordNeededError:
+            await client.sign_in(password=input('Password: '))
+
+    channel = input('Enter the link of ID corresponding to the channel: ')
+
+    if channel.isdigit():
+        entity = PeerChannel(int(channel))
+    else:
+        entity = channel
+
+    my_channel = await client.get_entity(entity)
+    limit = int(input("Enter limit (no of messages you want extracted): "))
+    offset_id = 0
+    all_messages = []
+    total = 1
+
+    while True:
+        print("Extracting Message", total)
+        try:
+            history = await client(GetHistoryRequest(peer=my_channel, offset_id=offset_id, offset_date=None, add_offset=0, limit=limit, max_id=0, min_id=0, hash=0))
+        except ChannelPrivateError:
+            print("Entered channel is private!")
+            break
+        if not history.messages or total == limit:
+            break
+        messages = history.messages
+        for message in messages:
+            if(str(message.message) == 'None'):
+                continue
+            #extracting the sender id and message alone
+            try:
+                message_info = {'from_id': str(message.from_id.user_id), 'message': str(message.message)}
+                all_messages.append(message_info)
+            except AttributeError:
+                message_info = {'from_id': None, 'message': str(message.message)}
+                all_messages.append(message_info)
+        
+        offset_id = messages[len(messages) - 1].id
+        total += 1
+
+    print(all_messages)
+
+    #writing to JSON
+    with open('channel_messages.json', 'w') as outfile:
+        json.dump(all_messages, outfile)
+
+    
+
+with TelegramClient(username, api_id=api_id, api_hash=api_hash) as client:
+    client.loop.run_until_complete(extract_messages(phone))
